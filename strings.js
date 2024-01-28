62af727e6413ea178a865ca9e6d1f238f851eb64
Cory <156404583+Samtron89@users.noreply.github.com>
2024-01-28 11:39:13 -0700

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,35 @@
+// Palindrome function using a string//
+function palindrome(str) {
+	const reversestring = str.split("").reverse().join("");
+	return reversestring === str;
+}
+// Main function for user input
+function palindromeWord() {
+	while (true) {
+		let user = prompt("Please enter in your word:");
+		if (user === null || user === "") {
+			alert("Thank you! Come back anytime!");
+			break; // Exits the Loop//
+		}
+
+		// To make sure string is a panlindrome//
+		let Pal = palindrome(user);
+
+		// Displays result to user//
+
+		let msg = Pal ? "Surpise! Palindrome!" : "Sorry it's not a Palindrome.";
+		alert(msg);
+
+		// Continue input or exit//
+
+		const result = confirm(" Do you wish to enter another word?");
+		if (!result) {
+			alert("Thank you for visiting my website!");
+			break;
+		}
+	}
+}
+
+// Call function back with button//
+
+document.getElementById("button").addEventListener("click", palindromeWord);
